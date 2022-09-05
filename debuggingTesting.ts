1253d7e8f7cd146cbac230c3d70dfc79e899fa68
TovahParnes <tovah@parnes.com>
2022-09-05 12:47:40 +0200

    updated testing file

--- debuggingTesting.ts
+++ debuggingTesting.ts
@@ -3,29 +3,19 @@ const { constructor } = require("typescript");
 class Greeter {
   greeting: string;
   greeting2: string;
-  greeting3: string;
-  greeting4: string;
 
-  constructor(
-    message: string, message2: string,
-              message3: string,
-    message4: string
-  ) {
+  constructor(message: string, message2: string) {
     this.greeting = message;
     this.greeting2 = message2;
-    this.greeting3 = message3;
-    this.greeting4 = message4;
   }
 
   greet() {
-    return (
-      "Hello, " +
-      this.greeting +
-      this.greeting2 +
-      this.greeting3 +
-      this.greeting4
-    );
+    return "Hello, " + this.greeting + this.greeting2;
   }
 }
 
-let greeter = new Greeter("world", "!", "!", "!");
+let greeter = new Greeter("world", "!");
+
+export class TestClass {
+  constructor(api: A, config: C, config: d, config: e, config: f) {}
+}
